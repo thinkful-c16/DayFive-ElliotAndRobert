d9035b73bf01d4c4c17240b0b2cb9c7d1d84acdd
Rich Greenhill <rgnet1@yahoo.com>
2017-11-03 08:00:04 -0700

    toggleItem listener; added js- classes to html

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 /* global $ */
 
-
 function generateItem(itemName) {
   return `
     <li>
-      <span class="shopping-item">${itemName}</span>
+      <span class="js-shopping-item shopping-item">${itemName}</span>
       <div class="shopping-item-controls">
-        <button class="shopping-item-toggle">
+        <button class="js-shopping-item-toggle shopping-item-toggle">
           <span class="button-label">check</span>
         </button>
         <button class="shopping-item-delete">
@@ -17,10 +16,25 @@ function generateItem(itemName) {
   `;
 }
 
-$('#js-shopping-list-form').on('submit', function(event) {
-  event.preventDefault();
-  const itemName = $('.js-shopping-list-entry').val();
-  console.log(itemName);
+function handleItemSubmit() {
+  $('#js-shopping-list-form').on('submit', function(event) {
+    event.preventDefault();
+    const itemName = $('.js-shopping-list-entry').val();
+    const itemElement = generateItem(itemName);
+    $('.js-shopping-list').append(itemElement);
+  });
+}
 
-});
+function handleToggleItem() {
+  $('.js-shopping-list').on('click', '.js-shopping-item-toggle', function(event) {
+    $(event.target).closest('li').find('span.js-shopping-item').toggleClass('shopping-item__checked');
+  });
+}
+
+function main() {
+  handleItemSubmit();
+  handleToggleItem();
+}
+
+$(main);
 
