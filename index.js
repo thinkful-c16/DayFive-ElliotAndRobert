2888ab44c07f0d692400f2f00976d25966f02729
Rich Greenhill <rgnet1@yahoo.com>
2017-11-03 07:29:22 -0700

    add item generator, add submit listener

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 /* global $ */
 
-console.log('hello');
+
+function generateItem(itemName) {
+  return `
+    <li>
+      <span class="shopping-item">${itemName}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>
+  `;
+}
+
+$('#js-shopping-list-form').on('submit', function(event) {
+  event.preventDefault();
+  const itemName = $('.js-shopping-list-entry').val();
+  console.log(itemName);
+
+});
 
